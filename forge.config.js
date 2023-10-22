f8d2ce559efb57b7c4c6d876a54354dcfe6f01f5
‎ <shanenprz@gmail.com>
2023-10-22 19:41:38 +0800

    front-end design

---
+++ forge.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: '@shanenprz',
+          name: 'imgshrink',
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
+}
--- main.js
+++ main.js
@@ -1,15 +1,102 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+
+//Set Environment
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false; 
+const isWin = process.platform === 'win32' ? true : false;
 
 let mainWindow; 
+let aboutWindow;
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         icon: `${__dirname}/assets/icons/256icon.png`,
-        width: 500,
+        width: 500, 
         height: 600,
+        resizable: isDev,
+        backgroundColor: 'white',
     });
     mainWindow.loadURL(`${__dirname}/app/index.html`);
 }
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+      title: 'About ImageShrink',
+      icon: `${__dirname}/assets/icons/256icon.png`,
+        width: 300, 
+        height: 300,
+        resizable: isDev,
+        backgroundColor: 'white',
+    })
 
-app.on('ready', createMainWindow);
+    aboutWindow.loadURL(`${__dirname}/app/about.html`)
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+
+  mainWindow.on('closed', () => mainWindow = null)
+});
+
+const menu = [
+  ...(!isWin ? [ 
+    {
+        label: 'ImageShrink', 
+        submenu: [
+          {
+            label: 'About',
+            click: createAboutWindow,
+          }
+      ],
+    }] :  
+    []),
+  
+  {
+    role: 'fileMenu',
+
+  },
+
+   ...(isWin ? [
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About',
+          click: createAboutWindow,
+        }
+      ]
+    }
+  ] : []),
+  
+  ...(isDev
+    ? [
+      {
+        label: 'Developer',
+        submenu: [
+          {role: 'reload'},
+          {role: 'forcereload'},
+          {type: 'separator'},
+          {role: 'toggledevtools'},
+        ],
+      },
+    ] : []
+  ),
+]
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow()
+    }
+  })
+
+app.on('window-all-closed', () => {
+  if (!isWin) {
+    app.quit()
+  }
+})
+
+app.allowRendererProcessReuse = true;
